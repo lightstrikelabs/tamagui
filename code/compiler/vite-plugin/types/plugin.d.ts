89d370dde088b19b7a29b2630a0acb6d87b1d9ad
Geoffrey Sechter <geoffrey@lightstrike.co>
2025-04-24 16:51:54 -0700

    chore: plugin types fix

--- code/compiler/vite-plugin/types/plugin.d.ts
+++ code/compiler/vite-plugin/types/plugin.d.ts
@@ -1,6 +1,6 @@
 import type { TamaguiOptions } from '@tamagui/static';
 import type { Plugin } from 'vite';
-export declare function tamaguiPlugin({ optimize, disableResolveConfig, disableThemesBundleOptimize, ...tamaguiOptionsIn }?: TamaguiOptions & {
+export declare function tamaguiPlugin({ optimize, disableResolveConfig, ...tamaguiOptionsIn }?: TamaguiOptions & {
     optimize?: boolean;
     disableResolveConfig?: boolean;
     disableThemesBundleOptimize?: boolean;
